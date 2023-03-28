33111fe355f77df9169914e0444055d4c04e92aa
cwjki <cwjki97@gmail.com>
2023-03-28 18:42:51 -0400

    adding some ids for the user story

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -2,40 +2,35 @@ export default function Clock() {
   return (
     <div className="container-fluid text-dark">
       <div className="row align-items-center vh-100">
-        <div className="col-md-6 col-12 mx-auto">
-
+        <div className="col-md-6 col-sm-8 col-12 mx-auto">
           <h1 className="text-center fw-semibold p-3">25 + 5 Clock</h1>
 
           <div className="card bg-secondary shadow-lg rounded-5">
             <div className="card-body">
-              {/* <div className="card-title text-center ">
-                
-              </div> */}
-
               <div className="row align-items-center text-center pt-4 pb-1">
-                <div className="col fs-4">Break Length</div>
-                <div className="col fs-4">Session Length</div>
+                <div id="break-label" className="col fs-4">Break Length</div>
+                <div id="session-label" className="col fs-4">Session Length</div>
               </div>
 
               <div className="row align-items-center text-center">
                 <div className="col">
                   <div className="row align-items-center justify-content-center">
-                    <button className="col btn border-0 ">
+                    <button id="break-decrement" className="col btn border-0 ">
                       <i className="bi bi-chevron-down fs-3"></i>
                     </button>
-                    <div className="col fs-3">6</div>
-                    <button className="col btn border-0">
+                    <div id="break-length" className="col fs-3">5</div>
+                    <button id="break-increment" className="col btn border-0">
                       <i className="bi bi-chevron-up fs-3"></i>
                     </button>
                   </div>
                 </div>
                 <div className="col">
                   <div className="row align-items-center justify-content-center">
-                    <button className="col btn border-0">
+                    <button id="session-decrement" className="col btn border-0">
                       <i className="bi bi-chevron-down fs-3"></i>
                     </button>
-                    <div className="col fs-3">6</div>
-                    <button className="col btn border-0">
+                    <div id="session-length" className="col fs-3">25</div>
+                    <button id="session-increment" className="col btn border-0">
                       <i className="bi bi-chevron-up fs-3"></i>
                     </button>
                   </div>
@@ -45,15 +40,15 @@ export default function Clock() {
                 <div className="col-8 mx-auto">
                   <div className="card bg-primary rounded-5 shadow text-dark">
                     <div className="card-body text-center">
-                      <div className="card-text fs-3 fw-bold">Session</div>
-                      <div className="card-title fs-1 fw-bolder">25:00</div>
+                      <div id="timer-label" className="card-text fs-3 fw-bold">Session</div>
+                      <div id="time-left" className="card-title fs-1 fw-bolder">25:00</div>
                       <div className="row align-items-center">
-                        <button className="col btn border-0" type="button">
+                        <button id="start_stop" className="col btn border-0" type="button">
                           <i className="bi bi-play fs-1 "></i>
                         </button>
-                        <button className="col btn border-0" type="button">
+                        {/* <button className="col btn border-0" type="button">
                           <i className="bi bi-pause fs-1"></i>
-                        </button>
+                        </button> */}
                         <button className="col btn border-0" type="button">
                           <i className="bi bi-arrow-repeat fs-1"></i>
                         </button>
