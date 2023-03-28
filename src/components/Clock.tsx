c65147ce6da53773bfedb24747450356e0cf666a
cwjki <cwjki97@gmail.com>
2023-03-28 11:12:51 -0400

    creating the clock structure

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -6,22 +6,56 @@ export default function Clock() {
           <div className="card">
             <div className="card-header text-center fs-2">25 + 5 Clock</div>
             <div className="card-body">
-              <div className="card-title">
-                <div className="row align-items-center text-center">
-                  <div className="col fs-3">Break Length</div>
-                  <div className="col fs-3">Session Length</div>
-                </div>
-                <div className="row align-items-center text-center">
-                  <div className="col border">
-                    <span></span>
-                    <i className="bi bi-arrow-up-circle-fill"></i>
-                    <p>6</p>
+              <div className="row align-items-center text-center">
+                <div className="col fs-3">Break Length</div>
+                <div className="col fs-3">Session Length</div>
+              </div>
+              <div className="row align-items-center text-center">
+                <div className="col border">
+                  <div className="row">
+                    <div className="col">
+                      <i className="bi bi-arrow-down-circle-fill"></i>
+                    </div>
+                    <div className="col">6</div>
+                    <div className="col">
+                      <i className="bi bi-arrow-up-circle-fill"></i>
+                    </div>
                   </div>
-                  <div className="col"></div>
+                </div>
+                <div className="col border">
+                  <div className="row">
+                    <div className="col">
+                      <i className="bi bi-arrow-down-circle-fill"></i>
+                    </div>
+                    <div className="col">6</div>
+                    <div className="col">
+                      <i className="bi bi-arrow-up-circle-fill"></i>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div className="card-text"></div>
+              <div className="row">
+                <div className="col-6 mx-auto">
+                <div className="card">
+                <div className="card-body text-center">
+                  <div className="card-text">
+                    Session
+                  </div>
+                  <div className="card-title">
+                    25:00
+                  </div>
+                  <div className="card-text">
+                  <i className="bi bi-play"></i>
+                  <i className="bi bi-pause"></i>
+                  <i className="bi bi-arrow-repeat"></i>
+                  </div>
+                </div>
+              </div>
+                </div>
+              </div>
+             
             </div>
+            <div className="card-text"></div>
           </div>
         </div>
       </div>
