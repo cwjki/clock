be0ac12c74a0261398f73433d0267cc95e1c67a3
cwjki <cwjki97@gmail.com>
2023-03-28 12:15:15 -0400

    adding the icons, working on the clock structure

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -12,48 +12,57 @@ export default function Clock() {
               </div>
               <div className="row align-items-center text-center">
                 <div className="col border">
-                  <div className="row">
+                  <div className="row align-items-center">
                     <div className="col">
-                      <i className="bi bi-arrow-down-circle-fill"></i>
+                      <button className="btn">
+                        <i className="bi bi-chevron-down fs-3"></i>
+                      </button>
                     </div>
                     <div className="col">6</div>
                     <div className="col">
-                      <i className="bi bi-arrow-up-circle-fill"></i>
+                    <button className="btn">
+                        <i className="bi bi-chevron-up fs-3"></i>
+                      </button>
                     </div>
                   </div>
                 </div>
                 <div className="col border">
-                  <div className="row">
+                  <div className="row align-items-center">
                     <div className="col">
-                      <i className="bi bi-arrow-down-circle-fill"></i>
+                    <button className="btn">
+                        <i className="bi bi-chevron-down fs-3"></i>
+                      </button>
                     </div>
                     <div className="col">6</div>
                     <div className="col">
-                      <i className="bi bi-arrow-up-circle-fill"></i>
+                    <button className="btn">
+                        <i className="bi bi-chevron-up fs-3"></i>
+                      </button>
                     </div>
                   </div>
                 </div>
               </div>
               <div className="row">
                 <div className="col-6 mx-auto">
-                <div className="card">
-                <div className="card-body text-center">
-                  <div className="card-text">
-                    Session
-                  </div>
-                  <div className="card-title">
-                    25:00
-                  </div>
-                  <div className="card-text">
-                  <i className="bi bi-play"></i>
-                  <i className="bi bi-pause"></i>
-                  <i className="bi bi-arrow-repeat"></i>
+                  <div className="card">
+                    <div className="card-body text-center">
+                      <div className="card-text">Session</div>
+                      <div className="card-title">25:00</div>
+                      <div className="card-text">
+                        <button className="btn" type="button">
+                          <i className="bi bi-play fs-1"></i>
+                        </button>
+                        <button className="btn" type="button">
+                          <i className="bi bi-pause fs-1"></i>
+                        </button>
+                        <button className="btn" type="button">
+                          <i className="bi bi-arrow-repeat fs-1"></i>
+                        </button>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-                </div>
-              </div>
-             
             </div>
             <div className="card-text"></div>
           </div>
