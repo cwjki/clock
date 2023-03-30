76e5c5aaf5332df8c2e0f0822747e600853844a4
cwjki <cwjki97@gmail.com>
2023-03-30 13:05:11 -0400

    creating the DateTimeDisplay component

---
+++ src/components/CountDownTimer.tsx
@@ -0,0 +1,50 @@
+import { useCountDown } from "../hooks/useCountDown";
+
+const ExpiredNotice = () => {
+  return (
+    <div className="expired-notice">
+      <span>Expired!!!</span>
+      <p>Please select a future date and time.</p>
+    </div>
+  );
+};
+
+const ShowCounter = ({
+  days,
+  hours,
+  minutes,
+  seconds,
+}: {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}) => {
+  return (
+    <div className="show-counter">
+      <a
+        href="https://tapasadhikary.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="countdown-link"
+      >
+        <DateTimeDisplay value={days} type={"Days"} isDanger={days <= 3} />
+        <p>:</p>
+        <DateTimeDisplay value={hours} type={"Hours"} isDanger={false} />
+        <p>:</p>
+        <DateTimeDisplay value={minutes} type={"Mins"} isDanger={false} />
+        <p>:</p>
+        <DateTimeDisplay value={seconds} type={"Seconds"} isDanger={false} />
+      </a>
+    </div>
+  );
+};
+
+const CountDownTimer = ({ targetDate }: { targetDate: Date }) => {
+  const [days, hours, minutes, seconds] = useCountDown(targetDate);
+
+  if (days + hours + minutes + seconds <= 0) {
+    return <ExpiredNotice />;
+  } else {
+  }
+};
